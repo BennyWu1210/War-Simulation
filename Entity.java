e4a39c117c9c6a605e347b6d9be564545397a480
bennywu2019 <benny.wu.new@gmail.com>
2022-04-14 13:31:24 -0400

    Modified entity class

--- Entity.java
+++ Entity.java
@@ -1,19 +1,70 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+import java.util.*;
 /**
- * Write a description of class Entity here.
+ * The Entity class controls most of the objects that will be displayed in the simulation.
+ * It implements gif methods as well as cost, health bar option, damage, upgrade, move, etc.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (Benny Wu) 
+ * @version April 14th, 2022
  */
 public class Entity extends Actor
 {
-    /**
-     * Act - do whatever the Entity wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    
+    // For gif images
+    protected boolean isGif;
+    protected List<GreenfootImage> gifImageList;
+    protected GifImage gifImage;
+    protected int gifCounter, gifIndex, gifChangeRate, gifCycle;
+    protected GreenfootImage image;
+    
+    // keep track of the duration
+    protected int actCounter;
+    
+    // coordinates and movements
+    protected double xPos, yPos;
+    protected double speed;
+    
+    public Entity(){
+        this.isGif = false;
+    }
+    
     public void act()
     {
         // Add your action code here.
     }
+    
+    /**
+     * Override getImage for gif purposes
+     */
+    public GreenfootImage getImage(){
+        return isGif ? gifImageList.get(gifIndex) : image;
+    }
+    
+    /**
+     * Return distance from a coordinate 
+     * 
+     * @return double  the distance from a location, as a double
+     */
+    public double getDistance(double x, double y){
+        return Math.sqrt(x * x + y * y);
+    }
+    
+    /**
+     * Return distance from an actor 
+     * 
+     * @return double  the distance from another actor, as a double
+     */
+    public double getDistance(Actor actor){
+        double x = actor.getX(), y = actor.getY();
+        return Math.sqrt(x * x + y * y);
+    }
+    
+    /**
+     * 
+     */
+    public void move(double x, double y){
+        // TODO
+    }
+    
+    
 }
