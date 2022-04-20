9e7233c054b67fcec115c1ecc43b49ed0afc730e
bennywu2019 <benny.wu.new@gmail.com>
2022-04-20 13:13:26 -0400

    Improved stats bar

--- Healer.java
+++ Healer.java
@@ -16,8 +16,8 @@ public class Healer extends Soldier
         getImage().scale(50, 52);
         
         // intialize hp bar
-        if (direction == 1) hpBar = new StatBar(100, 100, this, 30, 5, 35, Color.BLUE, Color.YELLOW, false);
-        else hpBar = new StatBar(100, 100, this, 30, 5, 35, Color.GREEN, Color.RED, false);
+        if (direction == 1) hpBar = new StatBar(100, 100, this, 30, 5, 35, Color.RED, new Color(255, 204, 203), false, Color.WHITE, 1);
+        else hpBar = new StatBar(100, 50, this, 30, 5, 35, Color.CYAN, new Color(202, 255, 255), false, Color.BLACK, 1);
         hpBar.initLevel(1, 10);
     }
  
