65d2d2eeed63566ed747c40f2048d21a5ad00017
ArgusFeng <angus.fengs@gmail.com>
2022-04-28 15:55:26 -0400

    Soldier class commented

--- Soldier.java
+++ Soldier.java
@@ -31,11 +31,12 @@ public abstract class Soldier extends Entity
     protected boolean attackingTower;
     protected Tower targetTower;
     /**
-     * Act - do whatever the Soldiers wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
+     * The superclass for all the NPC's inside the game. They share common features such as attacking, dying, getting hit, having health bars, etc.
+     * Hence, we put them under the Soldier class.
      */
     public Soldier(int direction, int maxHP)
     {
+        //Passes the direction to the Entity class
         super(direction);
         this.direction = direction; 
         this.side = direction;
@@ -48,28 +49,35 @@ public abstract class Soldier extends Entity
         this.damage = 15;
         this.level = 1;
         this.deathGold = 5;
-        //this.worldStat = worldStat;
+        
     }
+    /**
+     * Calls the Entity addedTowWorld method which flips the image if "direction" = -1
+     */
     
-
     public void addedToWorld(World w){
         super.addedToWorld(w);
+        //Adding an HP Bar
         w.addObject(hpBar, getX(), getY());
+        //Adding a level bar
         hpBar.initLevel(1, 17);
     }
     
     public void act()
     {
         
-        
+        //Every 200 acts/ attack. Adjusted with the attack speed variable
         if (timer % (200.0 / attackSpeed) == 0) attack();
         
+        //If you are set to attack the Crystal and are closer to the Crystal than any other enemy, move towards the Crystal
         if (attackingTower && getDistance(targetTower) > attackRange - 3){
             move(targetTower);
         } 
+        //If you are not set to attack the tower and have no soldier target, just move forward
         else if (!attackingTower){
             if (target == null || target.getWorld() == null){
                 move((int)((speed + 0.5) * direction));
+            //If you have a target, move towards the target
             } else if (getDistance(target) > attackRange - 3){
                 move(target);
             } 
@@ -80,20 +88,20 @@ public abstract class Soldier extends Entity
             return;
         }
         timer++;
-        // hpBar.update(hp);
-        /*
-        if (timer == timerTest){
-            hp -= 10;
-            timer = 0;
-        }
-        */
+        
+        //If you are dead, update the Statistic bar (the bar at the top displaying gold and kill count
+        //Different soldiers give different gold, denoted by the deathGold variable
+        //All soldiers give one kill
         if (hp <= 0){
             ((MyWorld)getWorld()).updateStatistic(direction, deathGold);
             die();
             return;
         }
         
+        //Every 600 acts, level up
+        //Leveling up increases stats 
         if (timer % 600 == 0){
+            //The font also becomes bigger
             hpBar.initLevel(++level, 17 + level * 2);
             this.speed *= 1.2;
             this.damage *= 1.2;
@@ -132,6 +140,10 @@ public abstract class Soldier extends Entity
         }
         
     }
+    /**
+     * Calling this method on an object decreases it's health by the parameter and spawns an effect like a sword slash or blood
+     */
+    
     public void getHit(double hp, Effect effect){
         
         this.hp -= hp;
@@ -155,14 +167,14 @@ public abstract class Soldier extends Entity
         return this.direction;
     }
     
+    /**
+     * Returns which side you are on
+     */
     public int getSide(){
         return side;
     }
     
-    protected boolean touchSoldier(){
-        //wat dis code?
-        return true;
-    }
+    
     
     //reminder to make this abstract
     
@@ -171,15 +183,21 @@ public abstract class Soldier extends Entity
         this.getWorld().addObject(new DeathEffect("BanditDead.png", direction), getX(), getY());
         removeSelf();
     }
-    
+    /**
+     * Remove yourself from the world
+     */
     public void removeSelf(){
         getWorld().removeObject(this);
     }
-    
+    /**
+     * Return true if you have full health
+     */
     public boolean hasFullHealth(){
         return this.hp == this.maxHP;
     }
-    
+    /**
+     * Heal the object being called on, used by the "Healer" soldier
+     */
     public void heal(double hp){
         if (this.hp + hp >= this.maxHP) this.hp = this.maxHP;
         else this.hp += hp;
